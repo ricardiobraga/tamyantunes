432f6718071f82728302ccf65b5c1240a004ddd4
Ricardo Braga <rcardiobraga@gmail.com>
2021-10-01 20:19:47 -0300

    problema com material-ui

--- src/views/Contato/index.js
+++ src/views/Contato/index.js
@@ -4,15 +4,15 @@ import './styles.css'
 import avatar from './assets/avatar.jpg'
 import footerImg from './assets/footer.png'
 
-
+import {Instagram, Email}  from '@material-ui/icons'
 
 const Gorro = () => {
     return (
         <div className="main-contato" id="contato">
             <div className="content-contato">
                 <img className="img-avatar" src={avatar} alt="avatart" />
-                <a href={"https://www.instagram.com/tamytheknitter/"} target="_blank"> @tamytheknitter </a>
-                <a> tamytheknitter@gmail.com</a>
+                <a href={"https://www.instagram.com/tamytheknitter/"} target="_blank"> <Instagram fontSize="inherit" /> @tamytheknitter </a>
+                <a><Email fontSize="inherit"/> tamytheknitter@gmail.com</a>
 
             </div>
             <div className="contato-footer">
