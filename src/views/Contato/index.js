11cfb0db6d3c1d792713f552138bbb1f81f68d0c
Ricardo Braga <rcardiobraga@gmail.com>
2021-10-01 21:56:52 -0300

    ajustes de layout

--- src/views/Contato/index.js
+++ src/views/Contato/index.js
@@ -12,9 +12,10 @@ const Gorro = () => {
             <div className="content-contato">
                 <img className="img-avatar" src={avatar} alt="avatart" />
                 <a href={"https://www.instagram.com/tamytheknitter/"} target="_blank"> <Instagram fontSize="inherit" /> @tamytheknitter </a>
-                <a href="mailto:tamytheknitter@gmail.com"><Email fontSize="inherit"/> tamytheknitter@gmail.com</a>
+                <a href="mailto:tamytheknitter@gmail.com" target="_blank"><Email fontSize="inherit"/> tamytheknitter@gmail.com</a>
 
             </div>
+            
             <div className="contato-footer">
 
             <img  src={footerImg} alt="footer" />
